96a3c7a7802bdcbba804093ad4282ceb6e39a5aa
phatboislym <phatboislym@protonmail.com>
2022-08-25 11:51:49 +0000

    changed print

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(00, 99):
-    print("{:02d}, ".format(i), end='')
-print("{:02d}".format(99))
+for i in range(0, 99):
+    print("{0:02d}".format(i), end=", ")
+print("{:d}".format(99))
