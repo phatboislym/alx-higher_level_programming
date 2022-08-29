a5a8b9826e559e7e5bfc977eb6d7476872f23e46
phatboislym <phatboilsym@protonmail.com>
2022-08-29 22:11:54 +0100

    add 101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    index = 0
+    new_string = ""
+    for letter in str:
+        if index != n
+        new_string += letter
+        letter += 1
+    return (new_string)
