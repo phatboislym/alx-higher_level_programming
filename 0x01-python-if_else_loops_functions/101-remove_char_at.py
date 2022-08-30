62e5e8f6ddd4e3e00eafe03ced316c390edea82a
Adewole Conde <101429938+phatboislym@users.noreply.github.com>
2022-08-30 01:14:25 +0100

    tweak

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,7 +4,7 @@ def remove_char_at(str, n):
     index = 0
     new_string = ""
     for letter in str:
-        if index != n
-        new_string += letter
-        letter += 1
+        if index != n:
+            new_string += letter
+            letter += 1
     return (new_string)
