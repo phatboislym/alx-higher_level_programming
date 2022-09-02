5b7f96a006d96cf118b75d1cb0163e0f7567d9a1
phatboislym <phatboislym@protonmail.com>
2022-09-02 13:07:22 +0000

    tweaked 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) < 1:
-        print(my_list)
+        print("{}".format(my_list))
     else:
         reversed_list = list(reversed(my_list))
         for index in range(len(reversed_list)):
