0533da9da5e843d76e8a646e2bf237bb1d52b2a0
phatboislym <phatboilsym@protonmail.com>
2022-09-03 01:16:38 +0100

    readd: 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for x in range(len(matrix[i])):
-            print("{}".format(matrix[i][x]), end="")
+            print("{:d}".format(matrix[i][x]), end="")
             if x != (len(matrix[i]) - 1):
                 print(" ", end="")
     print("")
