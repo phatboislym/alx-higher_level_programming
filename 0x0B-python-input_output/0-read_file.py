f2f7ccb98993fddb7d8c4fc79feee0f9a3f4d045
phatboislym <phatboislym@protonmail.com>
2022-09-28 00:05:10 +0100

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" function reads a text file (UTF8) and prints it to stdout """
+
+
+def read_file(filename=""):
+    """
+    function reads the contents of a file and prints to stdout
+
+    Args:
+
+    filename (str): name of file to be read
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            print(line)
+
+
+read_file("README.md")
