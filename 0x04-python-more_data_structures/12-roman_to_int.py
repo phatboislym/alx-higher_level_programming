b892985c1989925ecf4ac27f8f756495910799bd
Adewole <phatboislym@protonmail.com>
2022-11-10 14:17:39 +0100

    fix: pycodestyle check

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
         return (0)
 
     roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50,
-                         "C": 100, "D": 500, "M": 1000}
+                      "C": 100, "D": 500, "M": 1000}
     arabic_number = 0
     length = len(roman_string)
     for i in range(length):
