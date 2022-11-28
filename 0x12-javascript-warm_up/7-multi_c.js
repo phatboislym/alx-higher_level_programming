9c7cb0ed76077994573719ef4d8441317a43b09c
Adewole <phatboislym@protonmail.com>
2022-11-29 00:48:49 +0100

    add: task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argv = process.argv;
+if (!argv[2]) {
+  console.log('Missing number of occurrences');
+} else if (isNaN(Number(argv[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < Number(argv[2]); i++) {
+    console.log('C is fun');
+  }
+}
