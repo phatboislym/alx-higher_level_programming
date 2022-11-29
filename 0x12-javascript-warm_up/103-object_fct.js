c9a0ed30b8de494068ef1400cf2438c891a9ac9b
Adewole <phatboislym@protonmail.com>
2022-11-29 03:33:32 +0100

    fix: task 17

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,8 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function(){
-  this.value += 1; };
+myObject.incr = function () {
+  this.value += 1;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
