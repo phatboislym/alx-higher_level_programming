e3a65b1a07bf6b67b9d9c9fec20526173fd1533a
Adewole <phatboislym@protonmail.com>
2022-11-30 01:31:28 +0100

    add: task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+/*
+* class Square that defines a square and inherits from class Rectangle
+* the constructor take one argument: size
+*/
+
+class Square extends require('./5-square.js') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
