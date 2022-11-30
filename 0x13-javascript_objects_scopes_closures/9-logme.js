d96f51fc269c96480864ceaeb8feac9b38582e93
Adewole <phatboislym@protonmail.com>
2022-11-30 02:08:49 +0100

    fix: add comments

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
+/**
+ * unction that prints the number of arguments already printed and the new argument value
+ */
 let count = 0;
 function logMe (item) {
   console.log(`${count}: ${item}`);
