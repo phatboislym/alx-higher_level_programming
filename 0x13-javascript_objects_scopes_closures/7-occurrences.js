72df534fcfc617cdd40c77bd5da51db487fe558f
Adewole <phatboislym@protonmail.com>
2022-11-30 01:54:18 +0100

    add: task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+function nbOccurences (list, searchElement) {
+  const size = list.length;
+  let count = 0;
+  for (let i = 0; i < size; i++) {
+    if (searchElement === list[i]) {
+      count++;
+    }
+  }
+  return count;
+}
+module.exports = { nbOccurences };
