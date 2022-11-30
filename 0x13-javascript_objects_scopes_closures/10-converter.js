538ef4f496afe4b9bb51d8d11cfd8216683f1caf
Adewole <phatboislym@protonmail.com>
2022-11-30 02:13:53 +0100

    add: task 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+/**
+ * function that converts a number from base 10 to another base passed as argument
+ */
+
+function converter (base) {
+  return num => num.toString(base);
+}
+module.exports = { converter };
