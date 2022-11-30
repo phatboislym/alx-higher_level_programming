0d89de872ca8ba050be61fbf48c2560e6be3535b
Adewole <phatboislym@protonmail.com>
2022-11-30 02:00:27 +0100

    add: task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+/**
+ * function that returns the reversed version of a list
+ */
+
+function esrever (list) {
+  const desrever = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    desrever.push(list[i]);
+  }
+  return desrever;
+}
+module.exports = { esrever };
