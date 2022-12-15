93aef18b60e5627df2da7c6892d7707ced34be30
Adewole <phatboislym@protonmail.com>
2022-12-15 15:18:42 +0100

    fix: update script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # a Bash script that takes in a URL, sends a request to that URL using curl,
 #+ and displays the size of the body of the response in bytes
 
-curl -s "$1" | wc -c
+curl -sI "$1" | wc -c
