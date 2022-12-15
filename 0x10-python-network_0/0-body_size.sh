0ed8a6b104ec60311066354cdc1b7c8bbd846639
Adewole <phatboislym@protonmail.com>
2022-12-15 15:24:50 +0100

    fix: adjust script length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a request to that URL using curl,
-#+ and displays the size of the body of the response in bytes
-
+# a Bash script that takes in a URL, sends a request to that URL using curl, and displays the size of the body of the response in bytes
 curl -sI "$1" | grep 'Content-Length' | cut -d " " -f1  
