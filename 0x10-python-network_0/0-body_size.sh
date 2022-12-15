9cbb1010400b0acfed3b0fe564a8f63880205a58
Adewole <phatboislym@protonmail.com>
2022-12-15 15:34:41 +0100

    fix: tweak script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a request to that URL using curl, and displays the size of the body of the response in bytes
-curl -sI "$1" | wc -c  
+curl -s "$1" | wc -c  
