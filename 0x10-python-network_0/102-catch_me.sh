4f9a917de4d8cedde77ec4e5a800964cae953f3a
Adewole <phatboislym@protonmail.com>
2022-12-16 17:31:29 +0100

    fix: update script

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response. You are not allow to use echo, cat, etc. to display the final result
-curl -sLX PUT 0.0.0.0:5000 -d "You got me!"
+curl -sLX PUT -d "You got me!" 0.0.0.0:5000/catch_me
