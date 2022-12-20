248d70b7474ba11925f2ff63cadb8217d7224f8a
Adewole <phatboislym@protonmail.com>
2022-12-21 00:17:21 +0100

    fix: update script

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 a Python script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter
-The letter must be sent in the variable q, if no argument is given, set q=""
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+The letter must be sent in the variable q, if no argument is given, set q = ""
 If the response body is properly JSON formatted and not empty, display the id
 and name like this: [<id>] <name>
 Otherwise:
@@ -11,23 +11,23 @@ Display No result if the JSON is empty
 You must use the package requests and sys
 You are not allowed to import packages other than requests and sys
 """
-from sys import argv
-from requests import post
+import sys
+import requests
 
 
 if __name__ == "__main__":
-    if len(argv) == 1:
+    if len(sys.argv) == 1:
         parameter = ""
     else:
-        parameter = argv[1]
-    test = {"q": parameter}
+        parameter = sys.argv[1]
+    payload = {"q": parameter}
 
-    request = post("http://0.0.0.0:5000/search_user", data=test)
+    result = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        result = request.json()
-        if result == {}:
+        response = result.json()
+        if response == {}:
             print("No result")
         else:
-            print(f"[{result.get("id")}] {result.get("name")}")
+            print(f"[{response.get("id")}] {response.get("name")}")
     except ValueError:
         print("Not a valid JSON")
